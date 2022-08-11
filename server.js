185e7aad743dd215dc73fe140afc0b0b201a2e26
Deepti Tiwari <49716268+iDeeptiTiwari@users.noreply.github.com>
2022-08-11 16:45:09 +0530

    [Deepti, Jay, Lakshminarayan, Hareesh] Add functionality to create docker image from the user input.

--- server.js
+++ server.js
@@ -52,6 +52,34 @@ app.get('/deletecontainer',(req,res)=>{
 
 })
 
+app.get("/createimage", async (req, res) => {
+  var imageName = req.query.imagename;
+  var fileContent = req.query.dockerfilecontent;
+  console.log("imageName");
+  console.log(imageName);
+  console.log("fileContent");
+  console.log(fileContent);
+
+  q = 'echo "' + fileContent + '" >Dockerfile';
+  console.log(q);
+  await exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+  });
+  console.log("file is created");
+  q2 = "docker build -t " + imageName + " .";
+  await exec(q2, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    q3 = "docker images | grep " + imageName;
+    exec(q3, (err, stdout, stderr) => {
+      console.log(err);
+      console.log(stderr);
+      res.send("<pre>" + stdout + "</pre>");
+    });
+  });
+});
+
 
 app.get('/ps',(req,res) => {
 
